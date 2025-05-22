091890e1344702e57f1b088afdd9b7388f8af90b
victor marins <vtrm2005@gmail.com>
2025-05-22 08:23:56 -0300

    styles(layout): layout envolve o header e o app

---
+++ src/components/ProductGallery.tsx
@@ -0,0 +1,13 @@
+export function ProductGallery() {
+    return (
+        <div>
+            {/* <img 
+        src={"https://m.media-amazon.com/images/I/519bjoeFBTL._AC_SX679_.jpg"} 
+        className="shadow-2xl object-cover"
+        height={"35%"}
+        width={"35%"}
+        alt="img_product"
+      /> */}
+        </div>
+    )
+}
