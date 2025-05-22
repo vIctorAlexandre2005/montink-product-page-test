aab7c4ea8243bd54b92838a4f10b70887f1433e7
20051972mr@ <vtrm2005@gmail.com>
2025-05-21 21:10:32 -0300

    refactor(utils): separa logicas de persistencia no storage

---
+++ src/utils/storage.ts
@@ -0,0 +1,23 @@
+export function setWithTTL(key: string, value: any, ttlMinutes: number) {
+  const now = new Date();
+  const item = {
+    value,
+    expiry: now.getTime() + ttlMinutes * 60 * 1000,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+export function getWithTTL(key: string) {
+  const itemStr = localStorage.getItem(key);
+  if (!itemStr) return null;
+
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+
+  if (now.getTime() > item.expiry) {
+    localStorage.removeItem(key); // Expirou
+    return null;
+  }
+
+  return item.value;
+}
