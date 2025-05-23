179fe5923329009a9387adfeefd58b920c9d4d3f
victor marins <vtrm2005@gmail.com>
2025-05-23 15:54:54 -0300

    fix(header): corrige bordas do header para borda apenas no bottom

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen w-full">
-      <header className="w-full bg-white border-b border-2 p-2 border-slate-400">
+      <header className="w-full bg-white border-b p-2 border-slate-400">
         <h1 className="text-3xl font-bold animate-pulse">Montink</h1>
       </header>
       <main>{children}</main>
